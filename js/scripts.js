9ad825592e3c366c8f2ce066c6be8e9c915348bb
David Lewis and Roger Yee <roger@>
2018-01-08 11:22:07 -0800

    Add BMI function to js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
-var temperature = function(tempF) {
+
+/*var temperature = function(tempF) {
   return (tempF - 32) * (5 / 9);
 };
 
@@ -6,3 +7,14 @@ var tempF = parseInt(prompt("Please enter a temperature in Fahrenheit:"));
 
 
 alert(temperature(tempF) + "degrees celsius");
+
+*/
+
+var BMI = function(weight, height) {
+  return weight / (height * height);
+};
+
+var weight = parseInt(prompt("Please enter your weight in lbs: "));
+var height = parseInt(prompt("Please enter your height in inches: "));
+
+alert(BMI(weight, height));
